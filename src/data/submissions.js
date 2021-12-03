610bb6a5e2f70be11f78626fa0317a283b56ba6a
RajanYadav1162 <rajanyadav1162@gmail.com>
2021-12-03 21:38:59 +0530

    added optimised images

--- src/data/submissions.js
+++ src/data/submissions.js
@@ -5,8 +5,10 @@ const submissions = [
     description: 'On-demand sand castle construction expertise.',
     url: '#',
     votes: 16,
-    avatar: '/public/images/avatars/daniel.jpg',
-    submissionImage: '/public/images/submissions/image-yellow.png',
+    avatar:
+      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    submissionImage:
+      'https://images.unsplash.com/photo-1598915850252-fb07ad1e6768?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
   },
   {
     id: 2,
@@ -14,8 +16,10 @@ const submissions = [
     description: 'Earn points when your favorite politicians pass legislation.',
     url: '#',
     votes: 11,
-    avatar: '/public/images/avatars/kristy.png',
-    submissionImage: '/public/images/submissions/image-rose.png',
+    avatar:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    submissionImage:
+      'https://images.unsplash.com/photo-1572987957529-46f6bd4880cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDh8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
   },
   {
     id: 3,
@@ -23,8 +27,10 @@ const submissions = [
     description: 'We have your measurements and shipping address.',
     url: '#',
     votes: 17,
-    avatar: '/public/images/avatars/veronika.jpg',
-    submissionImage: '/public/images/submissions/image-steel.png',
+    avatar:
+      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    submissionImage:
+      'https://images.unsplash.com/photo-1638218022343-3158d732f5b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
   },
   {
     id: 4,
@@ -32,8 +38,10 @@ const submissions = [
     description: 'High-minded or absent-minded? You decide.',
     url: '#',
     votes: 9,
-    avatar: '/public/images/avatars/molly.png',
-    submissionImage: '/public/images/submissions/image-aqua.png',
+    avatar:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    submissionImage:
+      'https://images.unsplash.com/photo-1637194990554-ced1a7812b51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDh8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
   },
 ];
 export default submissions;
